5f371390c509895b3a2881840a6d819768fc068e
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-02-26 17:48:33 +0100

    public sym

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
-import { Server } from '@kiwilan/fastify-utils'
+import { symlink } from 'fs/promises'
+import { Dotenv, PathUtils, Server } from '@kiwilan/fastify-utils'
 import { fastifyStatic } from '@fastify/static'
 
 const server = Server.make()
 
 server.start({
-  callback: (fastify) => {
+  callback: async (fastify) => {
     const __filename = fileURLToPath(import.meta.url)
     const __dirname = dirname(__filename)
 
@@ -14,5 +15,11 @@ server.start({
       root: join(__dirname, 'public'),
       prefix: '/public',
     })
+
+    const dotenv = Dotenv.make()
+    if (!dotenv.system.IS_DEV) {
+      const root = PathUtils.getFromRoot('src/public')
+      await symlink(root, root.replace('src', 'build'))
+    }
   },
 })
