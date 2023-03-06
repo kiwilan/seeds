04645727c88d0c37fc78e0885b5b2a197256b795
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-03-06 14:26:39 +0100

    wip, add converter

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,6 @@
-import { Worker } from 'worker_threads'
 import { LocalServer } from '@kiwilan/fastify-utils'
 import fastifyStatic from '@fastify/static'
 import { FsFile, FsPath } from '@kiwilan/filesystem'
-
-/**
-* Use a worker via Worker Threads module to make intensive CPU task
-* @param filepath string relative path to the file containing intensive CPU task code
-* @return {Promise(mixed)} a promise that contains result from intensive CPU task
-*/
-const _useWorker = (filepath: string) => {
-  return new Promise((resolve, reject) => {
-    const worker = new Worker(filepath)
-
-    worker.on('online', () => {
-      console.log('Launching intensive CPU task')
-    })
-    worker.on('message', messageFromWorker => {
-      console.log(messageFromWorker)
-
-      return resolve
-    })
-
-    worker.on('error', reject)
-    worker.on('exit', code => {
-      if (code !== 0)
-        reject(new Error(`Worker stopped with exit code ${code}`))
-    })
-  })
-}
 
 LocalServer.run({
   apiKeyProtect: '/api',
@@ -43,11 +16,5 @@ LocalServer.run({
       await FsFile.deleteDirectory(symLinkPath)
       await FsFile.link(root, symLinkPath)
     }
-  },
-  afterStart: async () => {
-    // const path = dotenv.IS_DEV
-    //   ? FsPath.root('src/worker.ts')
-    //   : FsPath.root('build/worker.mjs')
-    // await _useWorker(path)
   },
 })
---
+++ src/thread.ts
@@ -0,0 +1,25 @@
+import { Worker, workerData } from 'worker_threads'
+
+/**
+* Use a worker via Worker Threads module to make intensive CPU task
+*/
+export const useWorker = (filepath: string): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(filepath)
+
+    worker.on('online', () => {
+      console.error('Launching intensive CPU task')
+    })
+    workerData.on('message', (messageFromWorker: any) => {
+      console.error(messageFromWorker)
+
+      return resolve
+    })
+
+    worker.on('error', reject)
+    worker.on('exit', code => {
+      if (code !== 0)
+        reject(new Error(`Worker stopped with exit code ${code}`))
+    })
+  })
+}
