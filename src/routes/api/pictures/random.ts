aacec9f04b46dce34a1403673e7a4a291ee5fced
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-03-08 08:57:47 +0100

    up random route

--- src/routes/api/pictures/random.ts
+++ src/routes/api/pictures/random.ts
@@ -6,14 +6,15 @@ const route: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.route({
     method: 'GET',
     url: Router.setRoute('/api/pictures/random'),
-    async handler() {
+    async handler(request, reply) {
       const service = await PictureService.make({
         size: 'medium'
       })
       const pictures = service.getPictures()
       const picture = pictures[Math.floor(Math.random() * pictures.length)]
+      const path = picture.pathFilename?.replace('large', 'medium')
 
-      return picture.links?.render
+      return reply.sendFile(path || '')
     },
   })
 }
