397b880c7e14852ba1bdf478969fec63642dcfcf
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-04-17 13:28:18 +0200

    wip

--- src/routes/api/pictures/random/[number].ts
+++ src/routes/api/pictures/random/[number].ts
@@ -1,15 +1,12 @@
 import type { FastifyPluginAsync } from 'fastify'
 import { Router } from '@kiwilan/fastify-utils'
 import { PictureService } from '~/services/PictureService'
-import { SharpService } from '~/services/SharpService'
-import type { Size } from '~/types'
 
 const route: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.route({
     method: 'GET',
     url: Router.setRoute('/api/pictures/random/:number'),
     async handler(request, reply) {
-      const params = request.params as { number: string }
       const queryParams = request.query as {
         category?: string
         size?: string
@@ -21,10 +18,11 @@ const route: FastifyPluginAsync = async (fastify): Promise<void> => {
       const pictures = service.getPictures()
       const picture = pictures[Math.floor(Math.random() * (pictures.length - 1))]
 
-      const size: Size = (queryParams.size as Size) || 'medium'
-      const sharp = SharpService.make(picture.pathFilename, size)
+      const path = picture.pathFilename.replace('/large/', `/${queryParams.size}/`)
+      // const size: Size = (queryParams.size as Size) || 'medium'
+      // const sharp = SharpService.make(picture.pathFilename, size)
 
-      return reply.sendFile(sharp.getNewPathOrigin())
+      return reply.sendFile(path)
     },
   })
 }
